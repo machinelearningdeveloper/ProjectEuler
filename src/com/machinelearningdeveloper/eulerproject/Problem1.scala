6507e3d14f457084b65f4403c3a4d8357d7a548e
machinelearningdeveloper <machinelearningdeveloper@gmx.com>
2014-01-25 19:35:34 -0600

    Add problem 1

---
+++ src/com/machinelearningdeveloper/eulerproject/Problem1.scala
@@ -0,0 +1,12 @@
+package com.machinelearningdeveloper.eulerproject
+
+/** Problem 1 http://projecteuler.net/problem=1
+  * "Find the sum of all the multiples of 3 or 5 below 1000."
+  */
+
+object Problem1 extends App {
+  val multiplesOf3Or5 =
+    for (i <- 1 until 1000 if (i % 3 == 0 || i % 5 == 0)) yield i
+    
+  println(multiplesOf3Or5 sum)
+}
