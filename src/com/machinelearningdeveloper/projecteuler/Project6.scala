ddc785c632736e3c93028d54083a544f2c369649
machinelearningdeveloper <tom@machinelearningdeveloper.com>
2014-01-29 20:47:43 -0600

    Add solution to Problem 6

---
+++ src/com/machinelearningdeveloper/projecteuler/Project6.scala
@@ -0,0 +1,21 @@
+package com.machinelearningdeveloper.projecteuler
+
+/** Problem 4 http://projecteuler.net/problem=6
+  *  
+  * "Find the difference between the sum of the squares of the
+  * first one hundred natural numbers and the square of the sum."
+  */
+
+object Project6 extends App {
+  
+  println(squareOfSumMinusSumOfSquares(100))
+  
+  def squareOfSumMinusSumOfSquares(n: Long) =
+    sum(n, afterSumF = square) - sum(n, perElementF = square)
+  
+  def sum(n: Long, perElementF: (Long => Long) = {x => x}, afterSumF: Long => Long = {x => x}) =
+    afterSumF((for (i <- 1L to n) yield perElementF(i)) sum)
+    
+  def square(x: Long) = x * x
+    
+}
