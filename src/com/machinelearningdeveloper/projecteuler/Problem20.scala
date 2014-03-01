6ae83c2b78976a81979f88aa78c85876234d07da
machinelearningdeveloper <tom@machinelearningdeveloper.com>
2014-03-01 17:18:52 -0600

    Made the solution to problem 20 more compact.

--- src/com/machinelearningdeveloper/projecteuler/Problem20.scala
+++ src/com/machinelearningdeveloper/projecteuler/Problem20.scala
@@ -4,9 +4,6 @@ package com.machinelearningdeveloper.projecteuler
   *  "Find the sum of the digits in the number 100!"
   */
 
-object Problem20 extends App {  
-  println(factorial(100).toString.map(_.asDigit).sum)
-  
-  def factorial(n: Int, result: BigInt = 1): BigInt =
-    if (n == 1) result else factorial(n - 1, result * n)
+object Problem20 extends App {
+  println((1 to 100).map(BigInt(_)).product.toString.map(_.asDigit).sum)
 }
