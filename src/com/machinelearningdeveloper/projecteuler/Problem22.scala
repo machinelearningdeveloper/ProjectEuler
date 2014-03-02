9221e9ccd4f6ddee29443ee997409d8726a9309f
machinelearningdeveloper <tom@machinelearningdeveloper.com>
2014-03-02 13:48:39 -0600

    Add solution to problem 22.

---
+++ src/com/machinelearningdeveloper/projecteuler/Problem22.scala
@@ -0,0 +1,25 @@
+package com.machinelearningdeveloper.projecteuler
+
+/** Problem 23 http://projecteuler.net/problem=22
+  * "What is the total of all the name scores in the file [resources/names.txt]?"
+  */
+
+object Problem22 extends App {
+  val collectionChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ,"
+  val namesSource = scala.io.Source.
+    fromFile("resources/names.txt")
+    
+  val names = namesSource.
+    getLines.toVector.mkString(", ").filter(collectionChars.contains(_)).
+    split(",").sorted.zipWithIndex
+    
+  namesSource.close
+  
+  val nameScores = names.map {
+    t =>
+      val (name, index) = t
+      name.map(c => c - 'A' + 1).sum * (index + 1)
+  }
+  
+  println(nameScores.reduce(_+_))
+}
