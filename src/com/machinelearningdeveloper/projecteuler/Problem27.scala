1fc58668cd713d8dafde77ed8a532cdbe031607a
machinelearningdeveloper <tom@machinelearningdeveloper.com>
2014-03-02 18:10:10 -0600

    Clean up output text to accurately describe what the program does.

--- src/com/machinelearningdeveloper/projecteuler/Problem27.scala
+++ src/com/machinelearningdeveloper/projecteuler/Problem27.scala
@@ -1,18 +1,32 @@
 package com.machinelearningdeveloper.projecteuler
 
 object Problem27 extends App {
-  val start = System.nanoTime
-  val coeffs = testCoefficients(-1000, 1000).sortBy(_._2).last._1
-  println("The sum of the coefficients a, b such that |a| < 1000 and |b| < 1000")
-  println("resulting in the longest number of consecutive leading primes when")
-  println(s"combined with n in n^2 + an + b is ${coeffs._1 * coeffs._2}")
-  println(s"Took ${(System.nanoTime - start) / 1000000} ms")
+  val startTime = System.nanoTime
+  val limit = 1000
+  val sharedTxt = "leading primes when\ncombined with n in n^2 + an + b"
+  val answer = testCoefficients(-limit, limit)
+  if (answer.isEmpty)
+    println(s"No coefficients in (-limit, limit) produce $sharedTxt.")
+  else {
+    val coeffs = answer.get
+    println(s"The product of the coefficients a, b such that |a| < $limit and |b| < $limit")
+    println(s"resulting in the longest number of consecutive $sharedTxt is ${coeffs._1 * coeffs._2}.")
+  }
+  println(s"Took ${(System.nanoTime - startTime) / 1000000} ms")
   
-  def testCoefficients(start: Int, end: Int) =
-    for {
-      a <- start + 1 until end
-      b <- start + 1 until end
-    } yield ((a, b), numLeadingPrimes(a, b))
+  def testCoefficients(start: Int, end: Int): Option[(Int, Int)] = {
+    @scala.annotation.tailrec
+    def findLongestRunOfLeadingPrimes(a: Int, b: Int, bestCoeffs: Option[(Int, Int)] = None, longestRun: Int = 0): Option[(Int, Int)] = 
+      if (a == end && b == end)
+        bestCoeffs
+      else {
+        val runSize = numLeadingPrimes(a, b)
+        val (bestAB, longestR) = if (runSize > longestRun) (Some((a, b)), runSize) else (bestCoeffs, longestRun)
+        val (nextA, nextB) = if (b == end) (a + 1, start + 1) else (a, b + 1)
+        findLongestRunOfLeadingPrimes(nextA, nextB, bestAB, longestR)
+      }
+    findLongestRunOfLeadingPrimes(start + 1, start + 1)
+  }
   
   def numLeadingPrimes(a: Int, b: Int): Int = {
     @scala.annotation.tailrec
